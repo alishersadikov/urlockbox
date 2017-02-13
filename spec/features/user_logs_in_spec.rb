73aaa67b31224880132925d26b0ca0096a9e42a4
Alisher Sadikov <alishersadikov@gmail.com>
2017-02-13 13:32:58 -0700

    complete basic auth with tests

--- spec/features/user_logs_in_spec.rb
+++ spec/features/user_logs_in_spec.rb
@@ -11,5 +11,19 @@ feature "user visits login path" do
     click_on "Login"
 
     expect(current_path).to eq(links_path)
+    expect(page).to have_content("Successfully logged in")
+    expect(page).to have_content("Logged in with #{user.email}")
+  end
+
+  scenario "logs in with incorrect credentials" do
+    user = User.create(email: "test@example.com", password: "password")
+
+    visit login_path
+
+    fill_in "session[email]", with: user.email
+    fill_in "session[password]", with: "wrong_password"
+    click_on "Login"
+
+    expect(page).to have_content("Something went wrong. Try again")
   end
 end
