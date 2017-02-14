51663ec17392531101f11ea42c56dcf8bfc173ad
Alisher Sadikov <alishersadikov@gmail.com>
2017-02-13 19:18:38 -0700

    update link

--- app/services/hotreads_service.rb
+++ app/services/hotreads_service.rb
@@ -1,6 +1,6 @@
 class HotreadsService
   def self.send_link(url)
-    conn = Faraday.new(url: "http://localhost:3000")
+    conn = Faraday.new(url: "https://hotreads.herokuapp.com/")
     conn.post '/api/v1/reads', {url: url}
   end
 end
